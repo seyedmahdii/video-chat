d18c3ca7f8cc76d07a014b208996e354e62579a7
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2024-11-28 23:04:23 +0330

    Add our own video stream

--- public/script.js
+++ public/script.js
@@ -1,13 +1,29 @@
 const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
+const myVideo = document.createElement("video");
+myVideo.muted = true; // Mute my mic to myself
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
 
 myPeer.on("open", (id) => {
   socket.emit("join-room", roomId, id);
 });
 
-socket.on("user-connected", (userId) => {
-  console.log("User connected with ID: " + userId);
-});
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
